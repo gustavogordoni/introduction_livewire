0dbaa2b77d025f86243089de920e7b3843266c2d
gustavogordoni <gustavogordoni1@gmail.com>
2025-04-12 20:31:40 -0300

    12: install and configure Laravel Jetstream

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tweets', ShowTweets::class)->name('tweets');
+Route::get('tweets', ShowTweets::class)->name('tweets')->middleware('auth');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
