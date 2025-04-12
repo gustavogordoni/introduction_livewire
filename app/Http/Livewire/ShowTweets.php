93cf6365c15772cf69a02a71f95e3778d9496daa
gustavogordoni <gustavogordoni1@gmail.com>
2025-04-12 19:47:16 -0300

    08: add func create ShowTweets

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -7,12 +7,26 @@ use App\Models\Tweet;
 
 class ShowTweets extends Component
 {
-    public $message = 'Apenas um teste';
+    public $message = 'Frase foda';
 
     public function render()
     {
         $tweets = Tweet::with('user')->get();
 
         return view('livewire.show-tweets', compact('tweets'));
+    }
+
+    public function create()
+    {
+        // dd($this->message);
+
+        Tweet::create(
+            [
+                'content' => $this->message,
+                'user_id' => 1,
+            ]
+        );
+
+        $this->message = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,12 +1,15 @@
 <div>
-    <h1>Show Tweets</h1>
+    <h1 class="text-3xl font-bold mb-3">Show Tweets</h1>
 
-    <p>{{ $message }}</p>
+    <p>Variável: {{ $this->message }}</p>
 
-    <input type="text" name="message" id="message" wire:model="message">
+    <form wire:submit.prevent="create" class="border border-gray-400 rounded p-2 my-4">
+        <input type="text" name="message" id="message" wire:model="message" class="border border-gray-400 rounded">
+        <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded">CriarTweet</button>
+    </form>
 
-    <hr>
 
+    <p class="text-2xl">Tweets:</p>
     @foreach ($tweets as $tweet)
     <p>{{ $tweet->user->name }} - {{ $tweet->content }}</p>
     @endforeach
