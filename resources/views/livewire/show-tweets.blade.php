f18b1e0b99ab6aa6b4f1b4132ba8c6a23babb41c
gustavogordoni <gustavogordoni1@gmail.com>
2025-04-12 17:58:23 -0300

    05: create component ShowTweets

---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>Show Tweets</h1>
+
+    <p>{{ $message }}</p>
+
+    <input type="text" name="message" id="message" wire:model="message">
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\{
+    ShowTweets
+};
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('tweets', ShowTweets::class)->name('tweets');
